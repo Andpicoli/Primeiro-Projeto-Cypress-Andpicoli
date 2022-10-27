254fc2ba07944dc383ddcda2ba4e67f50cce6b84
Andpicoli <andpicoli@gmail.com>
2022-10-27 19:33:19 -0300

    adicionando informações Dashboard Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "projectId": 'uqrvei',
     "viewportWidth": 1440,
     "viewportHeight": 900,
     "baseUrl": 'https://buger-eats-qa.vercel.app',
